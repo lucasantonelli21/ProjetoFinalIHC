f1a0f5e33e5277e81bdb5b1813146efb42c1b5d8
Lucas Antonelli <lucaslba2105@gmail.com>
2022-10-19 16:30:03 -0300

    Alterações no Cadastro de Avião

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+function openCadastroA(){
+    window.location = "cadastroAviao.html"
+    
+}
+
+function openCadastroV(){
+    window.location = "cadastroVoo.html"
+}
+
+function openCadastroOV(){
+    window.location = "cadastroOcupacao.html"
+}
+
+function openRelatorio(){
+    window.location = "relatorioVoo.html"
+}
